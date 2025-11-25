746776b1bb690730d7aedf1aeb8683d77c33ac98
brendajuarez26 <brendajuarez2602@gmail.com>
2025-11-25 04:45:53 -0300

    cambios

--- frontend/src/pages/ListaPrestamos.jsx
+++ frontend/src/pages/ListaPrestamos.jsx
@@ -1,23 +1,23 @@
 import { useEffect, useState } from "react";
 import PrestamoCard from "../components/PrestamoCard";
-import "../styles/ListaPrestamos.css";
 
 export default function ListaPrestamos() {
   const [prestamos, setPrestamos] = useState([]);
 
   useEffect(() => {
+    
     const guardados = JSON.parse(localStorage.getItem("prestamos")) || [];
     setPrestamos(guardados);
   }, []);
 
   return (
-    <div className="lista-prestamos-container">
-      <h2 className="titulo-lista">Préstamos Registrados</h2>
+    <div style={{ width: "400px", margin: "auto", marginTop: "40px" }}>
+      <h2>Préstamos Registrados</h2>
 
       {prestamos.length === 0 ? (
-        <p className="no-prestamos">No hay préstamos.</p>
+        <p>No hay préstamos.</p>
       ) : (
-        prestamos.map(p => <PrestamoCard key={p.id} prestamo={p} />)
+        prestamos.map((p) => <PrestamoCard key={p.id} prestamo={p} />)
       )}
     </div>
   );
